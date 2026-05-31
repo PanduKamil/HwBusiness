093a36da5c70b4a107d1febbfc182063bedd923d
pandukamil <pandu.kamil@gmail.com>
2026-05-31 14:53:02 +0700

    redesign Ui

--- src/main/java/GenerateHash.java
+++ src/main/java/GenerateHash.java
@@ -2,7 +2,7 @@
 import org.mindrot.jbcrypt.BCrypt;
 public class GenerateHash {
     public static void main(String[] args) {
-        System.out.println(BCrypt.hashpw("asadsdsadasasd", BCrypt.gensalt(12)));
+        System.out.println(BCrypt.hashpw("Panduak27", BCrypt.gensalt(12)));
     }
 } 
     
