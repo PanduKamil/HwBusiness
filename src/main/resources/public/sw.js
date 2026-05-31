6776125d218e0d864137fd75ad608daa5313a8c4
PanduKamil <136492241+PanduKamil@users.noreply.github.com>
2026-05-31 16:16:00 +0700

    Update cache sw.js

--- src/main/resources/public/sw.js
+++ src/main/resources/public/sw.js
@@ -5,7 +5,7 @@
  * Update CACHE_NAME whenever deploying a new version to invalidate old caches.
  */
 
-const CACHE_NAME = "HW-DATA-V12";
+const CACHE_NAME = "HW-DATA-V13";
 
 const STATIC_ASSETS = [
     "/",
